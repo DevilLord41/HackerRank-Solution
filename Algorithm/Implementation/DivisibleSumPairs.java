b78948972f61af0558659a875350b2f01817093b
DevilLord RedEagle <maulaxe@yahoo.com>
2018-04-22 00:16:55 +0700

    Passed

---
+++ Algorithm/Implementation/DivisibleSumPairs.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    /*
+     * https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+     */
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int count = 0;
+        for(int i = 0; i < n;i++) {
+        	for(int j = (i+1);j<n;j++) {
+        		if((ar[i] + ar[j]) % k == 0) count++;
+        	}
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = divisibleSumPairs(n, k, ar);
+        System.out.println(result);
+    }
+}
